93bcd61e3b041c3db5b6e35a86bbd2e6587c95c5
Inzamam nirob <nirobkhanariyan156@gmail.com>
2023-12-23 20:13:49 +0600

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import Data from './Components/Data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <nav className='container'>
+      <div className="logo">
+        <img  src="/images/brand_logo.png" alt="" srcset="" />
+      </div>
+
+
+      <ul>
+        <li href ="#">Home</li>
+        <li href ="#">Location</li>
+        <li href ="#">About</li>
+        <li href ="#">Contact</li>
+      </ul>
+      <button>Login</button>
+     
+     </nav>
+
+     <Data />
+  
     </div>
+  
+  
   );
 }
 
